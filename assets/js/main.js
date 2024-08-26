58f324bc625686ed0be56e6972ecb33d8f829d55
AhmedHassan537 <b-29607@student.usa.edu.pk>
2024-08-26 14:54:34 +0500

    javaScript added

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const teacher = [
+    { id:1, name: "Ahmed", age:20, subject:"english", address:{city:"lahore", state:"punjab"}},
+    { id:2, name: "Ali", age:22, subject:"math", address:{city:"lahore", state:"punjab"}},
+    { id:3, name: "Asfand", age:24, subject:"urdu", address:{city:"lahore", state:"punjab"}},
+    { id:4, name: "Ammar", age:26, subject:"chemitry", address:{city:"lahore", state:"punjab"}},
+    { id:5, name: "zain", age:28, subject:"physics", address:{city:"lahore", state:"punjab"}},
+]
+
+console.log(teacher.address)
